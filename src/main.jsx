cbce438171be81e6280c9c9b1218666914321c79
Leandro Castro <leandro_castro980@hotmail.com>
2024-04-27 01:30:49 -0300

    trampo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-// index.js
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
